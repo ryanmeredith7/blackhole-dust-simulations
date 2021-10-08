1040218512d24d1ae2ffb3a7938d67fcd5a416ba
Ryan Meredith <ryan.meredith@unb.ca>
2021-10-08 13:26:26 -0300

    Renamed files and minor changes

--- test2.m
+++ test2.m
@@ -4,11 +4,13 @@ x0 = 4;
 a0 = 5;
 dx = 0.01;
 dt = 0.001;
-n = 4000;
+n = 5000;
 
 % Midpoints of the cells.
 xs = (dx/2:dx:a0).';
 
+% Here we set up the initial conditions.
+% these have nothing to do with mass, I just needed to name it something
 mi = 8*pi*p0/3*x0^3 ./ xs .^ 3;
 mi(xs < x0) = 8*pi*p0/3;
 
@@ -23,14 +25,29 @@ bi = -asin(sqrt(mi - ai));
 bmid = (b(:,1:end-1) + b(:,2:end)) ./ 2;
 p = (diff(b, 1, 2) + diff(a, 1, 2) ./ sin(2 .* bmid)) ./ (-4*pi*dt);
 
-% Plays a short movie of one of the solution values.
-animate(xs, sqrt(1 - xs .^ 2 .* a));
+% Plays a short movie of the solution values.
+disp("Plotting a");
+animate(xs, a);
 disp("Done a");
 uiwait(gcf);
+
+disp("Plotting al");
+animate(xs, sqrt(1 - xs .^ 2 .* a));
+disp("Done al");
+uiwait(gcf);
+
+disp("Plotting b");
 animate(xs, b);
 disp("Done b");
 uiwait(gcf);
+
+disp("Plotting p");
 animate(xs(2:end) - dx/2, diff(xs .^ 3 .* (sin(b) .^ 2 + a)) ./ (8*pi*dx .* (xs(2:end) - dx/2) .^ 2), [0, 0.3]);
+disp("Done p");
+uiwait(gcf);
+
+disp("Plotting p");
+animate(xs, p, [0, 0.3]);
 disp("Done p");
 
 function animate(xs, y, l)
@@ -42,7 +59,7 @@ function animate(xs, y, l)
 
     n = size(y, 2);
 
-    for i = 1:n
+    for i = 1:5:n
         p.YData = y(:,i);
         drawnow;
     end
---
+++ testStep.m
@@ -0,0 +1,45 @@
+% Paramaters to set up the initial conditions and solver.
+p0 = 0.02;
+x0 = 4;
+a0 = 5;
+dx = 0.01;
+dt = 0.001;
+n = 30000;
+
+% Cell boundries
+x = (0:dx:a0).';
+xo2 = x ./ 2;
+% Cell midpoints
+xm = x(2:end) - dx/2;
+
+% Creates the initial condition vectors, first the outside case, the the inside
+% case.
+a1 = (x0 / a0 ./ xm) .^ 2;
+b1 = -asin(sqrt(8*pi*p0/3*x0^3 ./ xm .^ 3 - a1));
+
+a1(xm < x0) = a0 ^ -2;
+b1(xm < x0) = -asin(sqrt(8*pi*p0/3 - a0 ^ -2));
+
+plotf = @(a,b) a;
+
+d = plotf(a1, b1);
+p = plot(xm, d);
+xlim([0, a0]);
+ylim([0, 0.2]);
+
+for i = 1:n
+
+    [a2,b2] = solverStep(a1, b1, x0, dx, dt, x, xo2);
+
+    try
+        p.YData = plotf(a2, b2);
+        drawnow limitrate;
+    catch e
+        warning("Not able to update plot, assumed that user ended early.");
+        break
+    end
+
+    a1 = a2;
+    b1 = b2;
+
+end
