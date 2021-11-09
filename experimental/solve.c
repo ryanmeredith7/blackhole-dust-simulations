3d047feba0367c55404fab60879ff61a140d5f89
Ryan Meredith <ryan.meredith@unb.ca>
2021-11-09 18:04:09 -0400

    Fixed bugs

--- experimental/solve.c
+++ experimental/solve.c
@@ -1,6 +1,8 @@
-#include <stddef.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <math.h>
+
+#include "solve.h"
 
 void speed(double x, double bl, double br, double *sl, double *sr);
 
@@ -8,17 +10,17 @@ void speed(double const x, double const bl, double const br, double *sl,
         double *sr) {
 
     if (floor(bl / M_PI) < floor(br / M_PI)) {
-        *sl = x / 2 * sin(bl)^2 / (bl - br);
-        *sr = x / 2 * sin(br)^2 / (br - bl);
+        *sl = x / 2 * pow(sin(bl), 2) / (bl - br);
+        *sr = x / 2 * pow(sin(br), 2) / (br - bl);
     } else if (floor(bl / M_PI + 0.5) > floor(br / M_PI + 0.5)) {
-        *sl = x / 2 * cos(bl)^2 / (br - bl);
-        *sr = x / 2 * cos(br)^2 / (bl - br);
+        *sl = x / 2 * pow(cos(bl), 2) / (br - bl);
+        *sr = x / 2 * pow(cos(br), 2) / (bl - br);
     } else {
+        double s;
         if (bl == br) {
-            double s = x / 2 * sin(2*bl);
+            s = x / 2 * sin(2*bl);
         } else {
-
-            double s = x / 2 * (sin(br)^2 - sin(bl)^2) / (br - bl);
+            s = x / 2 * (pow(sin(br), 2) - pow(sin(bl), 2)) / (br - bl);
         }
         if (s < 0) {
             *sl = s;
@@ -31,7 +33,7 @@ void speed(double const x, double const bl, double const br, double *sl,
 
 }
 
-bool solveStep(intmax_t const n, double const a1[n], double const b1[n],
+bool solveStep(intmax_t n, double const a1[n], double const b1[n],
         double a2[n], double b2[n], double const x0, double const dx,
         double const dt) {
 
@@ -46,7 +48,7 @@ bool solveStep(intmax_t const n, double const a1[n], double const b1[n],
     double fa = sr * (ar - al);
     double fb = sr * (br - bl);
 
-    for (intmax_t i = 1; ++i; i < n) {
+    for (intmax_t i = 1; i < n; ++i) {
 
         al = a1[i-1];
         ar = a1[i];
@@ -56,7 +58,8 @@ bool solveStep(intmax_t const n, double const a1[n], double const b1[n],
         speed(x0 + i * dx, bl, br, &sl, &sr);
 
         a2[i-1] = al - dt * ((fa + sl*(ar - al))/dx + al*sin(2*bl));
-        b2[i-1] = bl - dt * ((fb + sl*(br - bl))/dx + 1.5*sin(bl)^2 + al/2);
+        b2[i-1] = bl - dt * ((fb + sl*(br - bl))/dx + 1.5*pow(sin(bl), 2)
+                + al/2);
 
         fa = sr * (ar - al);
         fb = sr * (br - bl);
@@ -66,36 +69,38 @@ bool solveStep(intmax_t const n, double const a1[n], double const b1[n],
     al = a1[n-1];
     bl = b1[n-1];
 
-    double sb2 = sin(bl)^2 - dx/(x0 + n*dx)*(3*sin(bl)^2 + al);
+    double sb2 = pow(sin(bl), 2) - dx/(x0 + n*dx)*(3*pow(sin(bl), 2) + al);
     if (-1 <= sb2 && sb2 <= 1) {
         br = -asin(sqrt(sb2));
     } else {
-        return false
+        return false;
     }
-    ar = al*(1 + 2*(br - bl)*sin(2*bl)/(3*sin(bl)^2 + al));
+    ar = al*(1 + 2*(br - bl)*sin(2*bl)/(3*pow(sin(bl), 2) + al));
 
-    speed(x + n * dx, bl, br, &sl, &sr);
+    speed(x0 + n * dx, bl, br, &sl, &sr);
 
     a2[n-1] = al - dt * ((fa + sl*(ar - al))/dx + al*sin(2*bl));
-    b2[n-1] = bl - dt * ((fb + sl*(br - bl))/dx + 1.5*sin(bl)^2 + al/2);
+    b2[n-1] = bl - dt * ((fb + sl*(br - bl))/dx + 1.5*pow(sin(bl), 2) + al/2);
 
-    return true
+    return true;
 
 }
 
-intmax_t solve(intmax_t const m, intmax_t const n, double const a0[m],
+intmax_t solve(intmax_t m, intmax_t n, double const a0[m],
         double const b0[m], double a[n][m], double b[n][m], double const x0,
         double const dx, double const dt) {
 
-    a[0] = a0;
-    b[0] = b0;
+    for (intmax_t i = 0; i < m; ++i) {
+        a[0][i] = a0[i];
+        b[0][i] = b0[i];
+    }
 
-    for (intmax_t i = 1; ++i; i < n) {
+    for (intmax_t i = 1; i < n; ++i) {
         if (!solveStep(m, a[i-1], b[i-1], a[i], b[i], x0, dx, dt)) {
-            return i
+            return i;
         }
     }
 
-    return n
+    return n;
 
 }
--- experimental/solve.h
+++ experimental/solve.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stddef.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 bool solveStep(
