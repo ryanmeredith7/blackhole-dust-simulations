5fb4737f207936bb52b689c79c1d0436c8142de0
Ryan Meredith <ryan.meredith@unb.ca>
2021-11-16 23:42:11 -0400

    Initial Commit

---
+++ randtest.m
@@ -0,0 +1,45 @@
+% Paramaters to set up the initial conditions and solver.
+p0 = 0.08;
+x0 = 4;
+a0 = 5;
+dx = 0.01;
+dt = 0.001;
+n = uint64(5000);
+
+% Midpoints of the cells.
+xs = (dx/2:dx:a0).';
+
+% Here we set up the initial conditions.
+% these have nothing to do with mass, I just needed to name it something
+mi = 8*pi*p0/3*x0^3 ./ xs .^ 3;
+mi(xs < x0) = 8*pi*p0/3;
+
+ai = smoothdata(randn(size(xs))./2, "gaussian", 50);
+
+bi = -asin(sqrt(mi - ai));
+
+% Calls the function that solves the equations.
+[a, b] = solver(ai, bi, 0, dx, dt, uint64(n));
+
+% Calculates the denssity.
+bmid = (b(:,1:end-1) + b(:,2:end)) ./ 2;
+p = (diff(b, 1, 2) + diff(a, 1, 2) ./ sin(2 .* bmid)) ./ (-4*pi*dt);
+
+speed = 5;
+
+% Plays a short movie of the solution values.
+figure(Name="Animation of a");
+animate(xs, a, speed);
+uiwait(msgbox("Press OK to continue.", "Done plotting a"));
+
+figure(Name="Animation of alpha");
+animate(xs, 1 - xs .^ 2 .* a, speed);
+uiwait(msgbox("Press OK to continue.", "Done plotting alpha"));
+
+figure(Name="Animation of beta");
+animate(xs, b, speed);
+uiwait(msgbox("Press OK to continue.", "Done plotting beta"));
+
+figure(Name="Animation of rho");
+animate(xs(2:end) - dx/2, diff(xs .^ 3 .* (sin(b) .^ 2 + a)) ./ (8*pi*dx .* (xs(2:end) - dx/2) .^ 2), speed, [0, 0.3]);
+uiwait(msgbox("Press OK to continue.", "Done plotting rho"));
---
+++ test3.m
@@ -0,0 +1,48 @@
+% Paramaters to set up the initial conditions and solver.
+p0 = 0.08;
+x0 = 4;
+a0 = 10;
+dx = 0.01;
+dt = 0.001;
+n = uint64(40000);
+
+% Midpoints of the cells.
+xs = (dx/2:dx:a0).';
+
+% Here we set up the initial conditions.
+% these have nothing to do with mass, I just needed to name it something
+mi = 8*pi*p0/3*x0^3 ./ xs .^ 3;
+mi(xs < x0) = 8*pi*p0/3;
+
+ai = zeros(size(xs));
+ai(xs < x0/4) = xs(xs < x0/4)./4;
+ai(x0/4 < xs & xs < 3*x0/4) = x0/8 - xs(x0/4 < xs & xs < 3*x0/4)./4;
+ai(3*x0/4 < xs & xs < x0) = xs(3*x0/4 < xs & xs < x0)./4 - x0/4;
+
+bi = -asin(sqrt(mi - ai));
+
+% Calls the function that solves the equations.
+[a, b] = solver(ai, bi, 0, dx, dt, n);
+
+% Calculates the denssity.
+bmid = (b(:,1:end-1) + b(:,2:end)) ./ 2;
+p = (diff(b, 1, 2) + diff(a, 1, 2) ./ sin(2 .* bmid)) ./ (-4*pi*dt);
+
+speed = 20;
+
+% Plays a short movie of the solution values.
+figure(Name="Animation of a");
+animate(xs, a, speed);
+uiwait(msgbox("Press OK to continue.", "Done plotting a"));
+
+figure(Name="Animation of alpha");
+animate(xs, 1 - xs .^ 2 .* a, speed);
+uiwait(msgbox("Press OK to continue.", "Done plotting alpha"));
+
+figure(Name="Animation of beta");
+animate(xs, b, speed);
+uiwait(msgbox("Press OK to continue.", "Done plotting beta"));
+
+figure(Name="Animation of rho");
+animate(xs(2:end) - dx/2, diff(xs .^ 3 .* (sin(b) .^ 2 + a)) ./ (8*pi*dx .* (xs(2:end) - dx/2) .^ 2), speed, [0, 0.3]);
+uiwait(msgbox("Press OK to continue.", "Done plotting rho"));
