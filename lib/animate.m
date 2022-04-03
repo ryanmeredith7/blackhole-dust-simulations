35a7eb2fc9b3f3a3f793f1a140b88f6e2de99176
Ryan Meredith <ryan.meredith@unb.ca>
2022-04-03 19:14:25 -0300

    Added recording functionality

--- lib/animate.m
+++ lib/animate.m
@@ -1,7 +1,7 @@
 % This function shows an animation of one or more data sets. Can be used to
 % animate more than one data set on top of each other by calling it as
 % animate(x, cat(3, y1, y2, ..., yn), rate).
-function animate(x, y, rate, limits)
+function animate(x, y, rate, limits, kw)
 
     arguments
 
@@ -17,6 +17,8 @@ function animate(x, y, rate, limits)
         % Optional input for the limits of the y axis
         limits (1,2) {mustBeReal} = [min(y, [], "all"), max(y, [], "all")]
 
+        kw.file (1,1) string
+
     end
 
     % Draws the initial plot
@@ -27,6 +29,14 @@ function animate(x, y, rate, limits)
     % Gets the length of the animation and the number of data sets to plot
     m = size(y, 2);
     n = size(y, 3);
+
+    if isfield(kw, "file")
+        video = VideoWriter(kw.file);
+        record = true;
+        open(video);
+    else
+        record = false;
+    end
 
     % Does the animation: updates the plots if the user didn't exit the plot,
     % and redraws the plot
@@ -41,10 +51,20 @@ function animate(x, y, rate, limits)
                     "Not able to update plot, assumed that user ended early.");
                 break
             else
+                if record
+                    close(video);
+                end
                 rethrow(e);
             end
         end
         drawnow;
+        if record
+            writeVideo(video, getframe(p.Parent.Parent));
+        end
+    end
+
+    if record
+        close(video);
     end
 
 end
