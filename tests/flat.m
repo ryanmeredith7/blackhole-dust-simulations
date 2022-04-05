5c3412dc266b55276c29738e2d82843d5f0f90e0
Ryan Meredith <ryan.meredith@unb.ca>
2022-04-05 01:46:23 -0300

    Added flat test case

---
+++ tests/flat.m
@@ -0,0 +1,43 @@
+% Paramaters to set up the initial conditions and solver.
+p0 = 0.02;
+x0 = 4;
+a0 = 5;
+dx = 0.01;
+dt = 0.001;
+n = uint64(30000);
+
+% Midpoints of the cells.
+xs = (dx/2:dx:a0).';
+
+% Creates the initial condition vectors, first the outside case, then the
+% inside case.
+ai = zeros(size(xs));
+bi = -asin(sqrt(8*pi*p0/3*x0^3 ./ xs .^ 3));
+bi(xs < x0) = -asin(sqrt(8*pi*p0/3));
+
+% Calls the function that solves the equations.
+[a, b] = solve(ai, bi, 0, dx, dt, n);
+
+% Calculates the denssity.
+bmid = (b(:,1:end-1) + b(:,2:end)) ./ 2;
+p = (diff(b, 1, 2) + diff(a, 1, 2) ./ sin(2 .* bmid)) ./ (-4*pi*dt);
+
+speed = 20;
+
+% Plays a short movie of one of the solution values.
+figure(Name="Animation of a");
+animate(xs, a, speed, [-0.01, 0.01]);
+uiwait(msgbox("Press OK to continue.", "Done plotting a"));
+
+figure(Name="Animation of alpha");
+animate(xs, 1 - xs .^ 2 .* a, speed, [0, 1.1]);
+uiwait(msgbox("Press OK to continue.", "Done plotting alpha"));
+
+figure(Name="Animation of beta");
+animate(xs, b, speed);
+uiwait(msgbox("Press OK to continue.", "Done plotting beta"));
+
+figure(Name="Animation of rho");
+animate(xs, p, speed, [0, 0.2]);
+uiwait(msgbox("Press OK to continue.", "Done plotting rho"));
+
